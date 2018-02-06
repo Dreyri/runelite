0536446f9cd3031e7a49e496de3f1bf672ead0a7
Dreyri <frederik.engels24@gmail.com>
2018-02-06 19:38:13 +0100

    construct adapter from either player or index

--- runelite-client/src/main/java/net/runelite/client/plugins/playerinfo/PlayerAdapter.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/playerinfo/PlayerAdapter.java
@@ -1,6 +1,7 @@
 package net.runelite.client.plugins.playerinfo;
 
 import com.google.common.eventbus.Subscribe;
+import net.runelite.api.Client;
 import net.runelite.api.Player;
 import net.runelite.api.events.GameTick;
 
@@ -9,13 +10,31 @@ import java.awt.Point;
 public class PlayerAdapter
 {
 	private Player player;
+	private int cacheIdx;
 
-	Point localTilePosition;
+	private Point localTilePosition;
+
+	/**
+	 * for internal use only
+	 * @param player
+	 * @param cacheIdx
+	 */
+	private PlayerAdapter(Player player, int cacheIdx)
+	{
+		this.player = player;
+		this.cacheIdx = cacheIdx;
+
+		this.localTilePosition = null;
+	}
 
 	public PlayerAdapter(Player player)
 	{
-		this.player = player;
-		this.localTilePosition = null;
+		this(player, player == null ? -1 : player.getCacheIndex());
+	}
+
+	public PlayerAdapter(Client client, int idx)
+	{
+		this(client.getPlayerFromCache(idx), idx);
 	}
 
 	@Subscribe
