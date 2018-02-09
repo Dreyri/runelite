9c8bc5dca49d296b42690c9e231eede010b6ff61
Dreyri <frederik.engels24@gmail.com>
2018-02-09 20:29:48 +0100

    create bufficon

--- runelite-client/src/main/java/net/runelite/client/plugins/buffbar/BuffIcon.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/buffbar/BuffIcon.java
@@ -1,5 +1,42 @@
 package net.runelite.client.plugins.buffbar;
 
-public class BuffIcon
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+
+public abstract class BuffIcon implements Buff, Comparable<BuffIcon>
 {
+	public static int BUFF_ICON_SIZE = 20;
+
+	private BuffPriority priority;
+	private BuffType type;
+	private Dimension dimension;
+
+	public BuffIcon(BuffPriority priority, BuffType type)
+	{
+		this.priority = priority;
+		this.type = type;
+		dimension = new Dimension(BUFF_ICON_SIZE, BUFF_ICON_SIZE);
+	}
+
+	public int getPriority()
+	{
+		return priority.getPriority();
+	}
+
+	public Dimension render(Graphics2D graphics)
+	{
+
+		return getDimension();
+	}
+
+	@Override
+	public int compareTo(BuffIcon o)
+	{
+		return this.getPriority() - o.getPriority();
+	}
+
+	public Dimension getDimension()
+	{
+		return dimension;
+	}
 }
--- runelite-client/src/main/java/net/runelite/client/plugins/buffbar/Buffbar.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/buffbar/Buffbar.java
@@ -1,19 +1,58 @@
 package net.runelite.client.plugins.buffbar;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.util.TreeSet;
 
 public class Buffbar
 {
-	private List<BuffIcon> buffIcons;
+	private TreeSet<BuffIcon> buffIcons;
+
+	private int buffsPerRow;
+
+	private Dimension dimension;
 
 	public Buffbar()
 	{
-		buffIcons = new ArrayList<>();
+		buffIcons = new TreeSet<>();
+		buffsPerRow = 5;
+
+		dimension = new Dimension(buffsPerRow * BuffIcon.BUFF_ICON_SIZE, 5 * BuffIcon.BUFF_ICON_SIZE);
 	}
 
 	public boolean addBuff(BuffIcon icon)
 	{
 		return this.buffIcons.add(icon);
+	}
+
+	public boolean removeBuff(BuffIcon icon)
+	{
+		return this.buffIcons.remove(icon);
+	}
+
+	public Dimension render(Graphics2D graphics, Point drawAt)
+	{
+		Graphics2D buffGraphics = (Graphics2D) graphics.create();
+		buffGraphics.translate(500, 400);
+
+		int i = 0;
+		for (BuffIcon buff : buffIcons)
+		{
+			if (buff.isActive())
+			{
+				buff.render(buffGraphics);
+				buffGraphics.translate(buff.getDimension().getWidth(), 0);
+			}
+		}
+
+		buffGraphics.dispose();
+
+		return this.getDimension();
+	}
+
+	public Dimension getDimension()
+	{
+		return this.dimension;
 	}
 }
