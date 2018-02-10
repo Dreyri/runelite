842b83ff46b6dfbbff859334b7fb2148605dc920
Dreyri <frederik.engels24@gmail.com>
2018-02-10 20:49:02 +0100

    resize icon

--- runelite-client/src/main/java/net/runelite/client/plugins/buffbar/BuffIcon.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/buffbar/BuffIcon.java
@@ -1,16 +1,18 @@
 package net.runelite.client.plugins.buffbar;
 
 import net.runelite.api.Point;
+import net.runelite.client.ui.FontManager;
 import net.runelite.client.ui.overlay.OverlayUtil;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class BuffIcon implements Comparable<BuffIcon>
 {
-	public static int BUFF_ICON_SIZE = 20;
+	public static int BUFF_ICON_SIZE = 25;
 
 	Buff buff;
 
@@ -18,11 +20,15 @@ public class BuffIcon implements Comparable<BuffIcon>
 	private BuffType type;
 	private Dimension dimension;
 
+	private Font font;
+
 	public BuffIcon(Buff buff, BuffPriority priority)
 	{
 		this.buff = buff;
 		this.priority = priority;
 		dimension = new Dimension(BUFF_ICON_SIZE, BUFF_ICON_SIZE);
+
+		font = Font.getFont(Font.MONOSPACED);
 	}
 
 	public int getPriority()
@@ -36,8 +42,10 @@ public class BuffIcon implements Comparable<BuffIcon>
 
 		Dimension imageSize = getScaledDimension(new Dimension(icon.getWidth(), icon.getHeight()), new Dimension(20, 20));
 
-		graphics.setColor(new Color(0, 255, 0, 80));
-		graphics.fillRect(1, 1, 19, 19);
+		//graphics.setColor(new Color(0, 255, 0, 80));
+		graphics.setColor(getColorForType(buff.getType()));
+
+		graphics.fillRect(0, 0, BUFF_ICON_SIZE, BUFF_ICON_SIZE);
 
 		graphics.setColor(Color.BLACK);
 		graphics.drawRect(0, 0, BUFF_ICON_SIZE, BUFF_ICON_SIZE);
@@ -49,8 +57,10 @@ public class BuffIcon implements Comparable<BuffIcon>
 
 		graphics.drawImage(icon, drawX, drawY, imageSize.width, imageSize.height, null);
 
-		graphics.setColor(Color.GREEN);
-		OverlayUtil.renderTextLocation(graphics, new Point(2, 19), buff.getText(), Color.WHITE);
+		Font font = FontManager.getRunescapeSmallFont().deriveFont(Font.BOLD, 14);
+		graphics.setFont(font);
+
+		OverlayUtil.renderTextLocation(graphics, new Point(2, BUFF_ICON_SIZE - 1), buff.getText(), Color.GREEN);
 		return getDimension();
 	}
 
@@ -99,5 +109,20 @@ public class BuffIcon implements Comparable<BuffIcon>
 		}
 
 		return new Dimension(new_width, new_height);
+	}
+
+	public static Color getColorForType(BuffType type)
+	{
+		switch (type)
+		{
+			case BUFF:
+				return new Color(0, 255, 0, 80);
+			case DEBUFF:
+				return new Color(255, 0, 0, 80);
+			case INFO:
+				return new Color(0, 0, 255, 80);
+		}
+
+		return Color.WHITE;
 	}
 }
